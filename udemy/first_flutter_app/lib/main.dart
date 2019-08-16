04b9deae52e6712ede119a4a96be6b85e726bd5d
nafi <nafidurmus07@gmail.com>
2019-08-16 14:31:56 +0300

    create hello world app

---
+++ udemy/first_flutter_app/lib/main.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+void main() {
+  runApp(
+    new Center(
+      child: new Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
